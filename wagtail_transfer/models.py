bd4c111148a0efab5a8faece783b54841f87f075
Ihor Voronin <VoroninIhor@gmail.com>
2024-12-16 14:19:33 +0200

    new: Add map_model method for mapping models based on configuration settings.

--- wagtail_transfer/models.py
+++ wagtail_transfer/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
@@ -35,10 +36,37 @@ def get_base_model(model):
     return return_model
 
 
+def get_model_mapping_config():
+    """
+    Get the model mapping configuration from the settings, or return an empty dict
+
+    The model mapping configuration is a dictionary that maps model paths to other model paths.
+    This allows to export a model and import it as a different model.
+
+    Example: {'app_name.model_name': 'another_app_name.another_model_name'}
+    """
+    model_mapping = getattr(settings, "WAGTAILTRANSFER_MODEL_MAPPING", {})
+    return model_mapping
+
+
+def map_model(model_path):
+    """
+    Map a model path to another model path using the model mapping configuration
+
+    If the model path is not in the mapping configuration, return the original model path
+    (e.g. for 'app_name.model_name', return 'another_app_name.another_model_name')
+    """
+    model_mapping = get_model_mapping_config()
+    return model_mapping.get(model_path, model_path)
+
+
 def get_model_for_path(model_path):
     """
     Given an 'app_name.model_name' string, return the model class
+
+    This function also supports mapping the model to another model using the model mapping configuration
     """
+    model_path = map_model(model_path)
     app_label, model_name = model_path.split('.')
     return ContentType.objects.get_by_natural_key(app_label, model_name).model_class()
 
